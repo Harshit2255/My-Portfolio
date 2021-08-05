4cc360cddbad9479fd42112c0eb08032378e2fbc
Harshit <solankiharshit82@gmail.com>
2021-08-05 15:31:14 +0530

    new change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
-STATIC_ROOT = BASE_DIR / 'static'
