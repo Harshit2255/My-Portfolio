31954afdfd5b5cb7b932ae8b8cac5f7a14708ebe
Harshit <solankiharshit82@gmail.com>
2021-08-05 18:54:33 +0530

    new change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Enter Your Secret Key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "harshit-solanki-portfolio.herokuapp.com"]
 
 
 # Application definition
